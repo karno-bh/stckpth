d14b0b7f56cd3576c09f2c81f515ecb6719f30bd
karno-bh <karno.bh@gmail.com>
2022-08-06 12:05:08 +0300

    _refactoring_: removing prints

--- aggregator/time_utils.py
+++ aggregator/time_utils.py
@@ -4,12 +4,10 @@ from datetime import datetime, timedelta
 def past_round_minute_range(time: datetime, delta=1):
     t_from = time - timedelta(minutes=delta, seconds=time.second, microseconds=time.microsecond)
     t_to = t_from + timedelta(minutes=1)
-    print(t_from, t_to)
     return range(int(t_from.timestamp()), int(t_to.timestamp()))
 
 
 def past_round_hour_range(time: datetime, delta=1):
     t_from = time - timedelta(hours=delta, minutes=time.minute, seconds=time.second, microseconds=time.microsecond)
     t_to = t_from + timedelta(hours=1)
-    print(t_from, t_to)
     return range(int(t_from.timestamp()), int(t_to.timestamp()))
